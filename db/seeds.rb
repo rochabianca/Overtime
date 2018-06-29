f4ddf7970629498546e6af1a68b329a9a6686804
Bianca <bianca.rocha1711@gmail.com>
2018-06-29 08:46:01 -0300

    tests for posts and post index updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 require 'faker'
 
 100.times do |post|
-  Post.create(date: Faker::Date.between(30.days.ago, Date.today), rationale: Faker::ChuckNorris.fact)
+  @user = User.create(email: Faker::Internet.email, password: "123456", password_confirmation: "123456", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  Post.create(date: Faker::Date.between(30.days.ago, Date.today), rationale: Faker::ChuckNorris.fact, user_id: @user.id)
 end
 
 puts "100 posts have been created"
--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,22 +1,34 @@
 require 'rails_helper'
 
 describe 'navigate' do
+  before do
+    @user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Tyrion", last_name: "Lannister")
+    login_as(@user, :scope => :user)
+  end
+
   describe 'index' do
-    it 'can be reached successfuly' do
+    before do
       visit posts_path
+    end
+
+    it 'can be reached successfuly' do
       expect(page.status_code).to eq(200)
     end
 
     it 'has a title of Posts' do
-      visit posts_path
       expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
     end
   end
 
   describe 'creation' do
     before do 
-      user = User.create(email: "test@test.com", password: "123456", password_confirmation: "123456", first_name: "Tyrion", last_name: "Lannister")
-      login_as(user, :scope => :user)
       visit new_post_path
     end
     it 'has a new form that can be reached' do
